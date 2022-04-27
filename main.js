cfa2422c0b39bc9127b6841b2158c7ab1e76d9aa
Natalia <Natalia@192.168.1.3>
2022-04-27 12:12:43 +0200

    form became a modalWindow

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 let btnsForOrder = document.querySelectorAll(".goods__button");
-let btnSubmit = document.querySelector(".form__button");
-let orderForm = document.querySelector(".form");
 
 for (let btnForOrder of btnsForOrder) {
-  btnForOrder.addEventListener("click", function () {
+  btnForOrder.addEventListener("click", function (e) {
+    e.preventDefault();
+    var modalName = this.getAttribute("data-modal");
+    var modalWindow = document.querySelector(
+      '.modal-window[data-modal="' + modalName + '"]'
+    );
+
+    modalWindow.classList.add("active");
+    overlay.classList.add("active");
+
     let goodName = btnForOrder.parentElement.querySelector(".goods__header").textContent;
     let goods = document.querySelectorAll("option");
 
     for (let good of goods) {
       if (good.value === goodName) {
         good.setAttribute("selected", "selected");
-        console.log(good);
       }
     }
-  }); //function click
+  }); // click on btn for order
 }
+
+let btnSubmit = document.querySelector(".form__button");
+let orderForm = document.querySelector(".form");
 
 orderForm.addEventListener("submit", formSend);
 
@@ -44,7 +53,7 @@ async function formSend(e) {
     alert("заполните обязательные поля");
     orderForm.classList.remove("_sending");
   }
-}
+} //send order
 
 function FormValidate(orderForm) {
   let errorCounter = 0;
@@ -67,7 +76,7 @@ function FormValidate(orderForm) {
     }
   }
   return errorCounter;
-}
+} //FormValidate
 
 function formAddError(input) {
   input.classList.add("_error");
@@ -80,3 +89,18 @@ function formRemoveError(input) {
 function telTest(input) {
   return !/^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(input.value);
 }
+
+let overlay = document.querySelector(".overlay-modal");
+
+overlay.addEventListener("click", function () {
+  document.querySelector(".modal-window.active").classList.remove("active");
+  this.classList.remove("active");
+}); // back of modalWindow
+
+let modalCloseBtns = document.querySelectorAll(".js-modal-close");
+modalCloseBtns.forEach(function (item) {
+  item.addEventListener("click", function () {
+    this.parentElement.classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
